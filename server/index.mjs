ff14e41552afb2a52d59e109c4405c24ae024cce
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-10-31 15:35:33 +0000

    Adding a SQLite3 database

--- server/index.mjs
+++ server/index.mjs
@@ -1,5 +1,4 @@
 import express from 'express';
-const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
 
@@ -7,9 +6,3 @@ app.listen(4000, () =>{
     console.log('Ready');
 })
 
-let database = new sqlite3.Database(':memory:', (err) =>{
-    if(err){
-        return console.error(err.message)
-    }
-    console.log('Database C0nnected')
-});
