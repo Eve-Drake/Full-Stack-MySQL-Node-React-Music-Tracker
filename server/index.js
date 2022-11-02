9ee3cb3c8e7c7bdc6d19dc8675eaf212a2d86f10
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-02 14:04:44 +0000

    Connecting Database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
+import sqlite3 from 'sqlite3'
 import express from 'express';
-
-
 const app = express();
 
+const db = new sqlite3.Database(':memory:', (err) =>{
+    if(err) {
+        return console.log(err.message)
+    }
+    console.log('Database Connected')
+})
+
+db.close((err)=>{
+    if(err){
+        console.log(err.message)
+    }
+    console.log('Database Connection Closed')
+})
 
 
 app.get('/', (req,res) => {
