0bdf916c7003a6f3ae3dcfe311ebc44607620188
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-02 13:52:27 +0000

    changing to module type

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 import express from 'express';
 
+
 const app = express();
+
+
+
+app.get('/', (req,res) => {
+    res.send('Incorrect Route')
+})
 
 app.listen(4000, () =>{
     console.log('Ready');
