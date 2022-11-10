c98099df50cd1d18c2990d5bd865de8dbfe143f7
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-10 14:26:17 +0000

    Creating and Altering data from node

--- server/index.js
+++ server/index.js
@@ -10,12 +10,15 @@ const db = new sqlite3.Database('./music-data.db', sqlite3.OPEN_READWRITE, (err)
     console.log('Database Connected')
 })
 
-sql = `INSERT INTO music(title, artist, album) VALUES (?, ?, ?)`
+//db.run('CREATE TABLE music (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, artist TEXT, album TEXT)')
 
-db.run(sql, ['Database Added Title', 'Database Added Artist', 'Database Addded Album'], (err) => {
-    if (err){
-        return console.log(err.message)
+//db.run('DROP TABLE music')
+
+db.run('INSERT INTO music(title, artist, album) VALUES(?, ?, ?,)', ['Node Added Title', 'Node Added Artist', 'Node Added Album'], (err) =>{
+    if(err){
+        return console.log(err.message);
     }
+    console.log('Row Added: ${this.lastID}');
 })
 
 db.close((err)=>{
