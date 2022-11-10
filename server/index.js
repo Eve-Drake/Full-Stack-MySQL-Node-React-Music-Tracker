7b21a60939c4966e633b23ab98f6714b902dd067
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2022-11-10 13:18:20 +0000

    Insert data into database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import sqlite3 from 'sqlite3'
 import express from 'express';
 const app = express();
+let sql = ``
 
 const db = new sqlite3.Database('./music-data.db', sqlite3.OPEN_READWRITE, (err) =>{
     if(err) {
@@ -10,6 +11,7 @@ const db = new sqlite3.Database('./music-data.db', sqlite3.OPEN_READWRITE, (err)
 })
 
 sql = `INSERT INTO music(title, artist, album) VALUES (?, ?, ?)`
+
 db.run(sql, ['Database Added Title', 'Database Added Artist', 'Database Addded Album'], (err) => {
     if (err){
         return console.log(err.message)
